87dc6d5abd18cdeedf58b764e081afac668acfca
赵金福 <zhaojinfu@conlin360.com>
2020-11-08 11:37:54 +0800

    创建main主页

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,22 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import Main from '../views/main.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'main',
+    component: Main
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/main',
+    name: 'main',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/main.vue')
   }
 ]
 
---
+++ src/views/main.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>esgjhdsgjkbdasgjeag</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scope></style>
