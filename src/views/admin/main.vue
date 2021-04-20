4a4c5d80839861d95c5bfcfe5a5dbf4ddf073a16
赵金福 <zhaojinfu@conlin360.com>
2021-04-20 17:35:17 +0800

    admin页面容器编写

--- src/views/admin/main.vue
+++ src/views/admin/main.vue
@@ -1,35 +1,31 @@
 <template>
   <div>
-    <el-container>
+    <el-container class="home_container">
       <!-- 顶部标题 -->
-      <el-header class="fs-xl">智慧图书自助服务系统后台管理</el-header>
-
+      <el-header class="fs-xl">智慧图书自助服务系统管理端</el-header>
       <el-container>
-        <!-- 侧边导航栏 -->
+        <!-- 左侧导航栏 -->
         <el-aside width="200px"
           ><el-menu :default-openeds="['1', '3']">
-            <!-- 图书管理 -->
             <el-submenu index="1">
               <template slot="title"
-                ><i class="el-icon-message"></i>图书管理</template
+                ><i class="el-icon-notebook-2"></i>图书管理</template
               >
               <el-menu-item-group>
-                <el-menu-item index="1-1">图书列表</el-menu-item>
+                <el-menu-item index="1-1">图书信息</el-menu-item>
               </el-menu-item-group>
             </el-submenu>
-            <!-- 用户管理 -->
             <el-submenu index="2">
               <template slot="title"
-                ><i class="el-icon-menu"></i>用户管理</template
+                ><i class="el-icon-user"></i>用户管理</template
               >
               <el-menu-item-group>
-                <el-menu-item index="2-1">用户列表</el-menu-item>
+                <el-menu-item index="2-1">用户信息</el-menu-item>
               </el-menu-item-group>
             </el-submenu>
-            <!-- 日志管理 -->
             <el-submenu index="3">
               <template slot="title"
-                ><i class="el-icon-setting"></i>日志管理</template
+                ><i class="el-icon-tickets"></i>日志管理</template
               >
               <el-menu-item-group>
                 <el-menu-item index="3-1">日志信息</el-menu-item>
@@ -37,7 +33,16 @@
             </el-submenu>
           </el-menu></el-aside
         >
-        <el-main>Main</el-main>
+        <!-- 主要内容 -->
+        <el-main
+          ><el-table :data="tableData">
+            <el-table-column prop="date" label="日期" width="140">
+            </el-table-column>
+            <el-table-column prop="name" label="姓名" width="120">
+            </el-table-column>
+            <el-table-column prop="address" label="地址">
+            </el-table-column> </el-table
+        ></el-main>
       </el-container>
     </el-container>
   </div>
@@ -46,12 +51,27 @@
 <script>
 export default {
   data() {
-    return {};
+    const item = {
+      date: "2016-05-02",
+      name: "王小虎",
+      address: "上海市普陀区金沙江路 1518 弄",
+    };
+    return {
+      tableData: Array(20).fill(item),
+    };
   },
 };
 </script>
 
 <style lang="less" scoped>
+.home_container {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+}
+
 .el-header {
   background-color: #b3c0d1;
   color: #333;
@@ -71,9 +91,5 @@ export default {
   color: #333;
   text-align: center;
   line-height: 160px;
-}
-
-body > .el-container {
-  margin-bottom: 40px;
 }
 </style>
